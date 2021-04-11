07426c82dac4b09768a042bd8865f2e5ffa39008
salauddinmd21 <salauddinbusiness86@gmail.com>
2021-04-11 20:14:14 +0600

    ema-john-server

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const bodyParser =require('body-parser')
+const cors =require('cors')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9yfqt.mongodb.net/ ${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+const port = 4000
+ 
+
+
+ 
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("EmaJohnStore").collection("productsCollection");
+   
+console.log(err)
+  app.post('/addProduct', (req,res)=>{
+      const products = req.body;
+      console.log(products)
+      productsCollection.insertMany(products)
+      .then(result=>{
+          console.log(result.insertedCount)
+          res.send(result.insertedCount>0)
+      })
+  })
+  app.get('/product',(req,res)=>{
+      productsCollection.find({})
+      .toArray((err, documents)=>{
+          res.send(documents)
+      })
+  })
+  app.get('/product/:key',(req,res)=>{
+      productsCollection.find({key:req.params.key})
+      .toArray((err, documents)=>{
+          res.send(documents[0]);
+      })
+  })
+  
+   
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Ema Watson')
+})
+
+app.listen(port, ()=>{
+    console.log('listening port 400')
+})
+ 
